c41dad6eec91e6061105e416a6e47a82552616ff
KristianS22R <142444308+Solhoej@users.noreply.github.com>
2023-09-12 18:00:15 +0200

    Added calculator

---
+++ Calculator/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Calculator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string value;
+            do
+            {
+                float result;
+                Console.WriteLine("Enter first number");
+                float num1 = float.Parse(Console.ReadLine());
+                Console.WriteLine("Enter second number");
+                float num2 = float.Parse(Console.ReadLine());
+                Console.WriteLine("Enter operator: (+, -, *, /)");
+                string operation = Console.ReadLine();
+
+                switch (operation)
+                {
+                    case "+":
+                        result = num1 + num2;
+                        Console.WriteLine("Addition: " + result);
+                        break;
+                    case "-":
+                        result = num1 - num2;
+                        Console.WriteLine("Subtraction: " + result);
+                        break;
+                    case "*":
+                        result = num1 * num2;
+                        Console.WriteLine("Multiplication: " + result);
+                        break;
+                    case "/":
+                        result = num1 / num2;
+                        Console.WriteLine("Divition: " + result);
+                        break;
+                    default:
+                        Console.WriteLine("Enter valid number");
+                        break;
+                }
+                Console.WriteLine("Do you want to continue (y/n)");
+                value = Console.ReadLine();
+            }
+            while (value == "Y" || value == "y");
+        }
+    }
+}
